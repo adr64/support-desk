8698a1166d99483f6e0cda4f1e9d802422bcdd2b
AMX1085LTECR003\acastrillo <Adrian Castrillo>
2023-01-31 14:54:43 -0600

    server.js fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware') 
+require('colors')
+require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 // Connect to database
 connectDB()
@@ -12,29 +12,25 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to support desk API'})
-})
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-if(process.env.NODE_ENV === 'production') {
-    // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
-    
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
-    app.get('/', (req, res) => {
-        res.status(200).json({message: 'Welcome to support desk API'})
-    })
-    
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
 }
 
 app.use(errorHandler)
